7095f25f5e74d5170307e68fe85f3468cb8102d2
Carlos Eduardo Gonçalves Santos <120467564+kadugz@users.noreply.github.com>
2025-10-30 12:13:31 -0300

    Add files via upload

---
+++ espresso/cuboctaedro_55_atomos/adsorption_001-ontop/job_platinum.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -n 16
+#SBATCH --ntasks-per-node=16
+#SBATCH -p batch-AMD
+#SBATCH --mem=64000mb
+#SBATCH -w work3
+
+module load quantum-espresso/v7.3.1 #-intel
+
+ulimit -s unlimited
+
+mpirun --version
+echo "#############"
+echo "##LIBRARIES##"
+echo "#############"
+echo $LD_LIBRARY_PATH
+echo "#############"
+echo "#EXECUTABLE##"
+echo "#############"
+which pw.x
+echo "#############"
+echo "###WRK DIR###"
+echo "#############"
+pwd
+
+mpirun -n 16 pw.x < espresso.pwi > espresso.pwo
+
